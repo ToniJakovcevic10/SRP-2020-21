3030e7f02e6dd6dc92c7dee86f4f44bb679a5a25
ToniJakovcevic <toni.jakovcevic10@gmail.com>
2020-12-01 21:50:35 +0100

    Refactor login to include real JWT

--- postgres-express-node/api/routes/login.js
+++ postgres-express-node/api/routes/login.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const LoginSchemas = require("./login.schemas");
+const loginSchemas = require("./login.schemas");
 const SchemaValidator = require("../middleware/validate");
-const LoginController = require("../controllers/login.controller");
+const loginController = require("../controllers/login.controller");
 
-SchemaValidator.addSchemas(LoginSchemas);
+SchemaValidator.addSchemas(loginSchemas);
 
 module.exports = (rootRouter) => {
   rootRouter.use("/", router);
-
   router.post(
     "/login",
     SchemaValidator.validate("login"),
-    LoginController.login
+    loginController.login
   );
 };
